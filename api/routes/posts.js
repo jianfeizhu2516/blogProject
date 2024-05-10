d88d44037f6cbe4e198c6990b20b75aab7ae4ddb
jianfeizhu2516 <jianfeizhu2516@gmail.com>
2024-05-10 14:33:01 -0400

    test12

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {addPost,getPosts,deletePost,updatePost,getSinglePost} from "../controllers/postController.js"
 const router = express.Router();
 //前端组件-index -> (auth)routes -> handler
+
 router.get("/getPosts",getPosts)
 
 router.get("/getSinglePost/:id", getSinglePost)
